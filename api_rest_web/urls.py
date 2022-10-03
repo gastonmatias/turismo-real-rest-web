273b6bb0d26b0ecff34f3c5eafee245d168e1f05
gastonmatias <g.matiasvillagra@gmail.com>
2022-10-03 14:12:54 -0300

    modified- endpoints renombradas

--- api_rest_web/urls.py
+++ api_rest_web/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('deptos/<int:id>', DeptoView.as_view(), name='depto_process'),
     #path('add_reservation/', AddReservation.as_view(), name='reservation_process'),
     path('reserve/', addReservation, name='reserve_process'),    
-    path('getService/', GetService.as_view(), name='getService'),
-    path('getFechasNoDisponibles/<int:id>', GetFechasNoDisponibles.as_view(), name='getFechasNoDisponibles'),
+    path('servicios_extra/', GetService.as_view(), name='getService'),
+    path('disponibilidad_depto/<int:id>', GetFechasNoDisponibles.as_view(), name='getFechasNoDisponibles'),
     path('', getRoutes)
 ]
